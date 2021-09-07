6351c17ee0f16eaf2606542a59def8a82b657e7d
dmitriydenisov <dmitriyd2009@yandex.ru>
2021-09-07 10:02:48 +0400

    сделал табы поиска

--- #src/js/main.js
+++ #src/js/main.js
@@ -12,3 +12,32 @@ const swiper = new Swiper(".banner-section__slider", {
         clickable: true,
       },
 });
+
+// табы поиска
+const tabsBtn = document.querySelectorAll('.search__tabs-item');
+const tabsItems = document.querySelectorAll('.search__content-item');
+
+tabsBtn.forEach(onTabCkick);
+
+function onTabCkick(item){  
+  item.addEventListener('click', function(){
+
+    let tabId = item.getAttribute('data-tab');
+    let currentTab = document.querySelector(tabId);
+
+    if(!item.classList.contains('search__tabs-item--active')){
+      tabsBtn.forEach(function(item){
+        item.classList.remove('search__tabs-item--active');
+      });
+  
+      tabsItems.forEach(function(item){
+        item.classList.remove('search__content-item--active');
+      });
+  
+      item.classList.add('search__tabs-item--active');
+      
+      currentTab.classList.add('search__content-item--active');
+    }
+  });
+}
+document.querySelector('.search__tabs-item').click();
