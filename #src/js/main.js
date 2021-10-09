406bb52fc76ca2158b56bd3eac8b77304ba62682
dmitriydenisov <dmitriyd2009@yandex.ru>
2021-10-09 21:11:31 +0400

    фильтрация товаров в каталоге

--- #src/js/main.js
+++ #src/js/main.js
@@ -12,12 +12,7 @@ const banner = new Swiper(".banner-section__slider", {
         clickable: true,
       },
 });
-// function slider() {
-//   const slider = this.closest('.swiper');
-//     if (!slider) {
-//       return;
-//     }
-// }
+
 const productSlider = new Swiper('.product-slider',{
   loop: true,
   speed: 900,
@@ -127,37 +122,62 @@ if(filterBlock){
 }
 
 //фильтр
-const checks = document.querySelectorAll('.aside-filter__input');
-const cards = document.querySelectorAll('.product-item__wrapper');
+// const checks = document.querySelectorAll('.aside-filter__input');
+// const cards = document.querySelectorAll('.product-item__wrapper');
 
-function filter (category, items) {
-  items.forEach((item) => {
-    const isItemFiltred = !item.classList.contains(category)
-    // `${item}[data-filter="${category}"]`
-    // const isShowAll = category.toLowerCase() === 'all'
-    if(isItemFiltred){
-      item.classList.add('animate')
-    }else{
-      item.classList.remove('animate')
-    }
-  })
-}
-checks.forEach((check) => check.addEventListener('click', checkedHandler))
+// function filter (category, items) {
+//   items.forEach((item) => {
+//     const isItemFiltred = !item.classList.contains(category)
+//     // `${item}[data-filter="${category}"]`
+//     // const isShowAll = category.toLowerCase() === 'all'
+//     if(isItemFiltred){
+//       item.classList.add('animate')
+//     }else{
+//       item.classList.remove('animate')
+//     }
+//   })
+// }
+// checks.forEach((check) => check.addEventListener('click', checkedHandler))
 
-function checkedHandler () {
-  if(this.checked){
-    const currentCategory = this.dataset.filter
-    filter(currentCategory, cards)
-  }
-}
+// function checkedHandler () {
+//   if(this.checked){
+//     const currentCategory = this.dataset.filter
+//     filter(currentCategory, cards)
+//   }
+// }
 
-cards.forEach((card) => {
-  card.ontransitionend = function () {
-    if(card.classList.contains('animate')){
-      card.classList.add('dn')
-    }
-  }
-})
+// cards.forEach((card) => {
+//   card.ontransitionend = function () {
+//     if(card.classList.contains('animate')){
+//       card.classList.add('dn')
+//     }
+//   }
+// })
+let filters = document.querySelectorAll('input[data-filter]');
+let all_cards = document.querySelectorAll('div[data-country]');
+
+
+if (filters !== null)
+    filters.forEach(function (filter) {
+        filter.addEventListener("change", function () {
+            let checkbox = document.querySelectorAll("input[type=checkbox]:checked");
+            const values = Array.from(checkbox).map(elem => {
+                return elem.dataset.filter;
+            })
+            console.log(values)
+            all_cards.forEach(function (card) {
+              console.log(values.includes(card.dataset.country))
+                if (!values.length || values.includes(card.dataset.country)) {
+                    card.classList.remove('animate')
+                    card.classList.remove('dn')
+                }else{
+                    card.classList.add('animate')
+                    card.classList.add('dn')
+                }
+            });
+            
+        });
+    });
 
 //скрипт мобильного меню
 const mobileBtn = document.querySelector('.menu__btn');
